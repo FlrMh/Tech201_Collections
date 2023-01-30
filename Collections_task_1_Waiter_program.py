c5cee06b2bf29b5490500a926a52e712c6cad923
FlrMh <florina.mihai110@gmail.com>
2023-01-30 16:41:41 +0000

    content added

---
+++ Collections_task_1_Waiter_program.py
@@ -0,0 +1,20 @@
+print("Welcome to Food Flow!")
+print("MENU")
+print("1.Starters")
+Starters = ["Spicy Chicken Wings", "BBQ Pork Ribs", "Tempura Prawns"]
+print(Starters)
+Starters_choice = input()
+print("2.Mains")
+Mains = ["Chicken Fried Rice", "Chicken with Cashew Nuts", "Panang Chicken Curry"]
+print(Mains)
+Mains_choice = input()
+print("3.Desserts")
+Desserts = ["Gelato(Strawberry/Mango/Passion Fruit)", "Chocolate/Coconut Lava Cake", "Passion Fruit Fingers"]
+print(Desserts)
+Dessert_choice = input()
+print("Drinks_selection")
+Drinks = ["Sprite", "Fanta", "J20 Mango", "Draught Lager", "Draught IPA", "Sauvignon Blanc", "Pinot Noir", "Long Island Ice Tea", "Espresso/Pornstar/Dry/Wet/Dirty Martini"]
+print(Drinks)
+Drinks_choice = input()
+print(f"Thank you for your order with Food Flow! To confirm your order, for Starters you selected the {Starters_choice}, for mains, you picked the {Mains_choice}, for your dessert you wanted the {Dessert_choice}, and lastly, for drinks, you selected {Drinks_choice}.")
+print("Enjoy your meal!")
