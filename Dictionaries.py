71ddf3ca2b951f46d4e6b59ec47c16399184c73c
FlrMh <florina.mihai110@gmail.com>
2023-01-30 15:44:41 +0000

    notes

---
+++ Dictionaries.py
@@ -0,0 +1,57 @@
+# Dictionaries
+
+# Dictionaries use key/value pairs
+# Key = reference to a particular object (columns/categories)
+# Value = data storage mechanism you want to use
+
+# Creating a dictionary
+# student_1 = {
+#     "name": "Susan",
+#     "stream": "DevOps",
+#     "completed_lessons": 4,
+#     "completed_lesson_names": ["variables", "data_types", "set_up"]
+# }
+#
+# # Access data within a dictionary
+#
+# print(student_1["stream"]) # DevOps
+#
+# # Access an element within a dictionary
+#
+# print(student_1["completed_lesson_names"][0]) # Name of the key from where you want a value, and the index of the value
+#
+# # Changing a value in a dictionary
+#
+# student_1["completed_lessons"] = 3 # or "three" if it was a string
+# print(student_1["completed_lessons"])
+#
+#
+# # Removing items from a dictionary
+#
+# student_1["completed_lesson_names"].remove("data_types")
+# print(student_1["completed_lesson_names"])
+#
+#
+# # Dictionary methods
+#
+# # Keys
+# print(student_1.keys()) # prints the keys in a dictionary
+#
+# # Get the value of a key back
+# print(student_1.get("name"))
+#
+# # Get the values of a dictionary
+# print(student_1.values())
+#
+# print(student_1.items()) # Outputs array of tuples with key and value pairs in dictionary
+#
+Employee_1 = {
+    "name": "Christine Johnson",
+    "age": "32",
+    "length_of_employment(years)": 2.5,
+    "hourly_rate(£)": 13.50,
+    "department(s)": ["FOH", "BOH"],
+    "student": True
+}
+
+print(Employee_1)
---
+++ Lists_and_tuples.py
@@ -0,0 +1,71 @@
+# Lists
+
+# Lists in Python are called Arrays in other languages.
+# They are all about data management.
+
+# Here is our first list:
+
+shopping_list = ["milk", "eggs", "bread", "fruits", "cheese"]
+#
+# print(type(shopping_list)) # <class 'list'>
+# print(shopping_list) # Print the data in raw version, not as a list
+#
+# print(shopping_list[0]) # milk(the first data added to the list)
+# print(shopping_list[3]) # fruits
+# print(shopping_list[-1]) # cheese
+
+
+# We can change a specific part of our list by re-writing a value in the list
+
+# shopping_list[0] = "sugar"
+# print(shopping_list[0]) # Sugar (milk will be overwritten by sugar)
+
+
+# Lists methods
+
+# Adding to a list
+shopping_list.append("vegetables")
+# print(shopping_list)
+# print(shopping_list[5]) # vegetables
+# print(len(shopping_list)) # Check the length of a variable
+
+
+# Removing from a list
+# shopping_list.remove("bread")
+# print(shopping_list)
+# print(len(shopping_list))
+
+
+# Removing the last entry in the list without specifying it
+# shopping_list.pop()
+# print(len(shopping_list))
+# print(shopping_list)
+# shopping_list.pop()
+# print(shopping_list)
+
+
+# Mixed data type lists
+mixture = [1, 2, 3.5, "one", "two", "three", "four"]
+print(mixture)
+
+
+# List slicing
+# print(mixture[1:3]) # the indexes of the items we want to separate
+# print(mixture[-2::]) # reverses the order
+# print(mixture[::2]) # bounces over the amount of indexes specified
+
+
+# Tuples
+
+# Exactly the same as lists, except they are immutable(cannot be changed/edited).
+# Tuples are useful for elements you want to make sure are not going to change.
+
+# essentials = ("bread", "eggs", "milk")
+# print(essentials)
+# print(essentials.count("bread"))
+# essentials.remove("bread") # Tuples do not have built-in methods and neither item assignments
+
+
+
+
+
